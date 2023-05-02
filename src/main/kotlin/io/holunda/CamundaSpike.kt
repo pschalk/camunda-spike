b903f18b649eee7a99db27bfda239bb9a7de716d
Patrick Schalk <patrick.schalk@holisticon.de>
2023-05-02 11:55:38 +0200

    Add dummy stuff

--- src/main/kotlin/io/holunda/CamundaSpike.kt
+++ src/main/kotlin/io/holunda/CamundaSpike.kt
@@ -1,7 +1,13 @@
 package io.holunda
 
+import io.github.oshai.KotlinLogging
+import org.camunda.bpm.engine.delegate.DelegateExecution
+import org.camunda.bpm.engine.delegate.JavaDelegate
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+
+private val logger = KotlinLogging.logger {}
 
 @Suppress("UNUSED_PARAMETER")
 fun main(args: Array<String>) {
@@ -9,4 +15,10 @@ fun main(args: Array<String>) {
 }
 
 @SpringBootApplication
-open class CamundaSpike
+open class CamundaSpike {
+
+    @Bean
+    open fun dummyDelegate() : JavaDelegate {
+        return JavaDelegate { _ -> logger.info("Dummy delegate invoked") }
+    }
+}
